d043f575be42e33f6974e070445edca66a1ff830
silvina-varela <varelasilvina@gmail.com>
2023-04-20 21:22:29 -0300

    project-config

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+    <div class="col-3">
+        
+        </div>
+        <div class="col-9">
+            
+        </div>
+    </div>
+</div>
+@endsection
